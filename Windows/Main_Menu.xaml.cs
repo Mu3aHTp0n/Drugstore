21bea3f503c55c0277ffdb406d9054d0531d108a
Mu3aHTp0n <kovalevartur05@gmail.com>
2023-10-31 02:52:28 +0300

    rename: Переименовал MainWindow в Autorization style(Autorization.xaml) Стилизовал окно авторизации add Main_Menu добавил пустое окно для главного меню

--- Windows/Main_Menu.xaml.cs
+++ Windows/Main_Menu.xaml.cs
@@ -10,17 +10,16 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace Drugstore.Windows
 {
     /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
+    /// Логика взаимодействия для Main_Menu.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Main_Menu : Window
     {
-        public MainWindow()
+        public Main_Menu()
         {
             InitializeComponent();
         }
